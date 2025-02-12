c6434988f7158481d013bc6dc5f9b059ab817660
SilverMedallion <liamlaverty6@gmail.com>
2025-02-12 20:20:40 +0000

    Initial commit of blank unreal 3rd person template

---
+++ Source/NetworkedTimeShooter/NetworkedTimeShooterGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "NetworkedTimeShooterGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ANetworkedTimeShooterGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ANetworkedTimeShooterGameMode();
+};
+
+
+
